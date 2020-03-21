04b58e44e1b972cfdad2f0904b1dd66fec3eab91
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-21 22:20:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
-$new_array = []
 
 def square_array(array)
   counter = 0
-
+  new_array = []
   while (array.length<counter) do
-    new_array<< 1
+    new_array.push(1)
     counter += 1
   end
   new_array
