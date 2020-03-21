a78ce919a6c134c4df504ba715f9dab5963b4e8b
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-21 22:04:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,7 @@ def square_array(array)
   while array.length < counter do
     array[counter] = array[counter] ** 2
     counter++
-  end  
+  end
 end
-
 #numbers = [1, 2, 3]
 #square_array(numbers)
